c1dfed55e8c134573a0f5cc844362dfd69127a39
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-06 21:30:43 +0530

    Create Services Section

---
+++ frontend/src/data/servicesData.jsx
@@ -0,0 +1,57 @@
+import { images } from "../assets/images/assestsImages";
+
+export const servicesData = [
+    {
+        title: "Application Development",
+        icon: images.Service1Image,
+        items: [
+            "Custom Software",
+            "Enterprise Software",
+            "Web Development",
+            "Mobile App Development",
+            "Integration Solutions",
+            "AI Development & Automation",
+            "Data Science",
+        ],
+    },
+    {
+        title: "BPO / KPO",
+        icon: images.Service2Image,
+        items: [
+            "Call Center Outsourcing",
+            "Finance & Accounting Outsourcing",
+            "Shared Services Outsourcing",
+            "Document Processing",
+            "Financial Consultants",
+            "Business Operations (Management Consulting)",
+        ],
+    },
+    {
+        title: "IT Infrastructure",
+        icon: images.Service3Image,
+        items: [
+            "CCTV Solutions",
+            "Structured Cabling",
+            "Switching & Routing",
+            "Network Security Solutions",
+            "Data Centers",
+            "Virtualization",
+            "Storage Infrastructure",
+            "High Availability & Contingency Planning",
+        ],
+    },
+    {
+        title: "Digital Marketing",
+        icon: images.Service4Image,
+        items: [
+            "UI/UX Design",
+            "Social Media Marketing",
+            "Paid Marketing Campaigns",
+            "Email Campaigns",
+            "Instant Messaging Campaigns",
+            "Tele-Marketing Campaigns",
+            "Graphic Designing",
+            "Videography",
+        ],
+    },
+];
