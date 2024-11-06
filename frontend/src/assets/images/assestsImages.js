850edeab4bd902d562cd24eba0bc7a3fc6d82627
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-06 11:22:20 +0530

    Create success stories section

--- frontend/src/assets/images/assestsImages.js
+++ frontend/src/assets/images/assestsImages.js
@@ -1,6 +1,9 @@
 import HeroPicture from './Hero Picture.png'
+import BackgroundImage from './Background Picture.jpg'
+import DirectorImage from './Director Picture.jpg'
 
 export const images = {
     HeroPicture,
-
+    BackgroundImage,
+    DirectorImage,
 }
