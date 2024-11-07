532e101cd16a06610ce9ef68eac72d873fb3be1b
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-07 19:30:32 +0530

    Update Service Section with Animated Icons

--- frontend/src/component/SharedComponents/ServiceCard.jsx
+++ frontend/src/component/SharedComponents/ServiceCard.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { motion } from "framer-motion";
 
-const ServiceCard = ({ title, icon, items }) => {
+const ServiceCard = ({ title, icon, items, itemsIcons }) => {
     return (
         <motion.div 
             initial={{ y: "100%", opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1, ease: "easeOut" }}
-            className="flex flex-col justify-center items-center h-[450px] lg:h-[500px] text-white">
+            className="flex flex-col justify-center items-center h-[520px] lg:h-[580px] text-white">
             <div className='w-[100px] h-[100px] rounded-full mb-4'>
                 <img src={icon} alt={`${title} Icon`} className="rounded-full" />
             </div>
-            <div className='w-[50%] h-[70%] sm:w-[40%] lg:w-[85%] xl:w-[70%] xl:h-[65%] bg-[#ADBBDA] bg-opacity-[40%] border-solid border-2 border-white rounded-md text-center py-5 px-2'>
-                <h3 className="text-[18px] font-bold mb-4">
+            <div className='w-[50%] h-[70%] sm:w-[40%] lg:w-[85%] xl:w-[70%] xl:h-[65%] bg-[#ADBBDA] bg-opacity-[40%] border-solid border-2 border-white rounded-md py-5 px-2'>
+                <h3 className="text-[18px] font-bold mb-4 text-center">
                     {title}
                 </h3>
                 <ul className="text-[12px] sm:text-sm space-y-[8px] w-full font-semibold">
-                    
-                    {items.map((item, index) => (
-                        <li key={index} className="flex justify-center">
-                            <span>{item}</span>
+                    {itemsIcons.map((icon, index) => (
+                        <li key={index} className="flex items-center space-x-2">
+                            <img src={icon} alt={`Icon ${index}`} className="w-7 h-7" />
+                            <span>{items[index]}</span>
                         </li>
                     ))}
                 </ul>
--- frontend/src/data/servicesData.jsx
+++ frontend/src/data/servicesData.jsx
@@ -1,4 +1,5 @@
 import { images } from "../assets/images/assestsImages";
+import { gifs } from "../assets/Gifs/assetsGifs";
 
 export const servicesData = [
     {
@@ -13,6 +14,15 @@ export const servicesData = [
             "AI Development & Automation",
             "Data Science",
         ],
+        itemsIcons: [
+            gifs.Service1Icon1,
+            gifs.Service1Icon2,
+            gifs.Service1Icon3,
+            gifs.Service1Icon4,
+            gifs.Service1Icon5,
+            gifs.Service1Icon6,
+            gifs.Service1Icon7
+        ]
     },
     {
         title: "BPO / KPO",
@@ -25,6 +35,14 @@ export const servicesData = [
             "Financial Consultants",
             "Business Operations (Management Consulting)",
         ],
+        itemsIcons: [
+            gifs.Service2Icon1,
+            gifs.Service2Icon2,
+            gifs.Service2Icon3,
+            gifs.Service2Icon4,
+            gifs.Service2Icon5,
+            gifs.Service2Icon6,
+        ]
     },
     {
         title: "IT Infrastructure",
@@ -39,6 +57,16 @@ export const servicesData = [
             "Storage Infrastructure",
             "High Availability & Contingency Planning",
         ],
+        itemsIcons: [
+            gifs.Service3Icon1,
+            gifs.Service3Icon2,
+            gifs.Service3Icon3,
+            gifs.Service3Icon4,
+            gifs.Service3Icon5,
+            gifs.Service3Icon6,
+            gifs.Service3Icon7,
+            gifs.Service3Icon8,
+        ]
     },
     {
         title: "Digital Marketing",
@@ -53,5 +81,15 @@ export const servicesData = [
             "Graphic Designing",
             "Videography",
         ],
+        itemsIcons: [
+            gifs.Service4Icon1,
+            gifs.Service4Icon2,
+            gifs.Service4Icon3,
+            gifs.Service4Icon4,
+            gifs.Service4Icon5,
+            gifs.Service4Icon6,
+            gifs.Service4Icon7,
+            gifs.Service4Icon8,
+        ]
     },
 ];
