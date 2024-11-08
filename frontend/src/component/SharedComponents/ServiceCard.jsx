04f4f133ea708aa82eaba29e19cf87cd7d7c86a4
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-08 09:44:03 +0530

    Update Service Section Responsive

--- frontend/src/component/SharedComponents/ServiceCard.jsx
+++ frontend/src/component/SharedComponents/ServiceCard.jsx
@@ -8,7 +8,7 @@ const ServiceCard = ({ title, icon, items, itemsIcons }) => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 1, ease: "easeOut" }}
             className="flex flex-col justify-center items-center h-[520px] lg:h-[580px] text-white">
-            <div className='w-[100px] h-[100px] rounded-full mb-4'>
+            <div className='w-[100px] h-[100px] xl:w-[120px] xl:h-[120px] rounded-full mb-4'>
                 <img src={icon} alt={`${title} Icon`} className="rounded-full" />
             </div>
             <div className='w-[50%] h-[70%] sm:w-[40%] lg:w-[85%] xl:w-[70%] xl:h-[65%] bg-[#ADBBDA] bg-opacity-[40%] border-solid border-2 border-white rounded-md py-5 px-2'>
