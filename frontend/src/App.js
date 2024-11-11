e4356ef175a68288f7b06935fa1d08c558aa7940
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-11 18:42:33 +0530

    Remove sections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import NavBar from "./component/SharedComponents/NavBar";
-import Footer from "./component/SharedComponents/Footer";
-
 
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <Footer></Footer>
     </div>
   )
 }
