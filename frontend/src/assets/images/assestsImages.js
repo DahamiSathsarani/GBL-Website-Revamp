2148666fd217785972d4083866ffd6e892460406
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-11 14:53:43 +0530

    Add Partners Section Images and Logos

--- frontend/src/assets/images/assestsImages.js
+++ frontend/src/assets/images/assestsImages.js
@@ -7,6 +7,17 @@ import Service2Image from './Service 2.jpg'
 import Service3Image from './Service 3.jpg'
 import Service4Image from './Service 4.jpg'
 import ShapeImage from './Shape.png'
+import WaveImage from './Wave.png'
+import PartnersBackground from './PartnersBg.png'
+import PartnersBackgroundSm from './PartnersBgSm.png'
+import Partner1coloredImage from './partners-logos/Partner1-colored.png'
+import Partner2coloredImage from './partners-logos/Partner2-colored.png'
+import Partner3coloredImage from './partners-logos/Partners3-colored.png'
+import Partner4coloredImage from './partners-logos/Partners4-colored.png'
+import Partner1Image from './partners-logos/Partner1.png'
+import Partner2Image from './partners-logos/Partner2.png'
+import Partner3Image from './partners-logos/Partners3.png'
+import Partner4Image from './partners-logos/Partners4.png'
 
 
 export const images = {
@@ -18,5 +29,16 @@ export const images = {
     Service2Image,
     Service3Image,
     Service4Image,
-    ShapeImage
+    ShapeImage,
+    WaveImage,
+    PartnersBackground,
+    PartnersBackgroundSm,
+    Partner1coloredImage,
+    Partner2coloredImage,
+    Partner3coloredImage,
+    Partner4coloredImage,
+    Partner1Image,
+    Partner2Image,
+    Partner3Image,
+    Partner4Image,
 }
