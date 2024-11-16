5cf1fa92b5ff365123fc7e7fd20c083866f36119
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-16 11:07:50 +0530

    Create Services Pages

---
+++ frontend/src/pages/DigitalMarketing.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SubServicesSection from '../component/OurServicesPageComponents/SubServicesSection'
+
+const DigitalMarketing = () => {
+  return (
+    <SubServicesSection serviceKey="service4"/>
+  )
+}
+
+export default DigitalMarketing
---
+++ frontend/src/pages/ItInfrastructure.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SubServicesSection from '../component/OurServicesPageComponents/SubServicesSection'
+
+const ItInfrastructure = () => {
+  return (
+    <SubServicesSection serviceKey="service3"/>
+  )
+}
+
+export default ItInfrastructure
