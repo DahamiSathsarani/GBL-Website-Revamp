40f940aab78dc878f0174aa89ce3c019c83f176c
Dahami Sathsarani <dahamisathsarani99@gmail.com>
2024-11-18 15:47:28 +0530

    Create service pages hero section

--- frontend/src/pages/ItInfrastructure.jsx
+++ frontend/src/pages/ItInfrastructure.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import SubServicesSection from '../component/OurServicesPageComponents/SubServicesSection'
+import React from "react";
+import SubServicesSection from "../component/OurServicesPageComponents/SubServicesSection";
+import MainLayout from "../component/SharedComponents/MainLayout";
+import SubHeroSection from "../component/SharedComponents/sub-hero-section";
+import { images } from "../assets/images/assestsImages";
 
 const ItInfrastructure = () => {
-  return (
-    <SubServicesSection serviceKey="service3"/>
-  )
-}
 
-export default ItInfrastructure
+  const descriptionElement = (
+    <div className="xs:px-8 px-14 md:px-24 lg:px-0 lg:ml-[5rem] xl:ml-[6rem] 2xl:ml-[8rem]">
+      <p className="text-gradient  text-center lg:text-justify xs:text-[16px] text-[18px] xl:text-[22px] 2xl:text-[24px] font-sans">
+      Build a robust IT foundation with our comprehensive infrastructure solutions. 
+      From network management to cloud services, we ensure your business stays connected, secure, and future-ready.
+      </p>
+    </div>
+  );
+
+  const ProductSectionDescription = {
+    topic: "IT Infrastructure",
+    description: descriptionElement,
+    picture: images.Service3Image,
+    upperIcon: images.ApplicationDevelopmentUpperIcon,
+    lowerIcon: images.ApplicationDevelopmentLowerIcon,
+  };
+
+  return (
+    <MainLayout>
+      <SubHeroSection {...ProductSectionDescription}></SubHeroSection>
+      <SubServicesSection serviceKey="service3" />
+    </MainLayout>
+  );
+};
+
+export default ItInfrastructure;
+
